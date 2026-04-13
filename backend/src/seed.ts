bafa400a137cccb9f9bcb296309ea7623125bdbf
Carlos Anjos <carlosotacilio65@gmail.com>
2026-04-13 19:25:28 -0300

    feat: adicionado seed para criação automática do admin

---
+++ backend/src/seed.ts
@@ -0,0 +1,31 @@
+import { prisma } from "./database/prisma";
+import bcrypt from "bcrypt";
+
+async function seed() {
+  const adminExists = await prisma.user.findFirst({
+    where: {
+      email: "admin@admin.com"
+    }
+  });
+
+  if (!adminExists) {
+    const password = await bcrypt.hash("admin123", 10);
+
+    await prisma.user.create({
+      data: {
+        name: "Admin",
+        email: "admin@admin.com",
+        password,
+        role: "ADMIN"
+      }
+    });
+
+    console.log("✅ Admin criado");
+  } else {
+    console.log("⚠️ Admin já existe");
+  }
+}
+
+seed()
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());
